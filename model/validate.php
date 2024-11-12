d2420148c4473dd67dfd96e646b77f3bb6c6d24e
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-12 17:46:23 -0600

    Add step 8

--- model/validate.php
+++ model/validate.php
@@ -85,57 +85,11 @@ class Validate {
         $this->text($name, $value, $required);
         if ($field->hasError()) { return; }
 
-        // Split email address on @ sign and check parts
-        $parts = explode('@', $value);
-        if (count($parts) < 2) {
-            $field->setErrorMessage('At sign required.');
-            return;
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            $field->setErrorMessage('Invalid email address.');
+        } else {
+            $field->clearErrorMessage();
         }
-        if (count($parts) > 2) {
-            $field->setErrorMessage('Only one at sign allowed.');
-            return;
-        }
-        $local = $parts[0];
-        $domain = $parts[1];
-
-        // Check lengths of local and domain parts
-        if (strlen($local) > 64) {
-            $field->setErrorMessage('Username part too long.');
-            return;
-        }
-        if (strlen($domain) > 255) {
-            $field->setErrorMessage('Domain name part too long.');
-            return;
-        }
-
-        // Patterns for address formatted local part
-        $atom = '[[:alnum:]_!#$%&\'*+\/=?^`{|}~-]+';
-        $dotatom = '(\.' . $atom . ')*';
-        $address = '(^' . $atom . $dotatom . '$)';
-
-        // Patterns for quoted text formatted local part
-        $char = '([^\\\\"])';
-        $esc  = '(\\\\[\\\\"])';
-        $text = '(' . $char . '|' . $esc . ')+';
-        $quoted = '(^"' . $text . '"$)';
-
-        // Combined pattern for testing local part
-        $localPattern = '/' . $address . '|' . $quoted . '/';
-
-        // Call the pattern method and exit if it yields an error
-        $this->pattern($name, $local, $localPattern,
-                'Invalid username part.');
-        if ($field->hasError()) { return; }
-
-        // Patterns for domain part
-        $hostname = '([[:alnum:]]([-[:alnum:]]{0,62}[[:alnum:]])?)';
-        $hostnames = '(' . $hostname . '(\.' . $hostname . ')*)';
-        $top = '\.[[:alnum:]]{2,6}';
-        $domainPattern = '/^' . $hostnames . $top . '$/';
-
-        // Call the pattern method
-        $this->pattern($name, $domain, $domainPattern,
-                'Invalid domain name part.');
     }
 
     public function password($name, $password, $required = true) {
